a502c57029558921d9d8700e01ee54e5c984b881
Joao Lucas Melo <h4yko@Macbook.local>
2026-04-16 18:20:51 -0300

    supabase db

--- dashboard/app.py
+++ dashboard/app.py
@@ -13,8 +13,12 @@ DB_USER = os.getenv('DB_USER')
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
 
-engine = create_engine(f'postgresql://{DB_USER}:@{DB_HOST}:{DB_PORT}/{DB_NAME}')
+engine = create_engine(
+    f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}',
+    connect_args={"sslmode": "require"}
+)
 
 st.set_page_config(page_title='Premier League Dashboard', page_icon='⚽')
 st.title('📊 Premier League 2025/2026')
--- database/load.py
+++ database/load.py
@@ -8,8 +8,12 @@ DB_USER = os.getenv('DB_USER')
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
 
-engine = create_engine(f'postgresql://{DB_USER}:@{DB_HOST}:{DB_PORT}/{DB_NAME}')
+engine = create_engine(
+    f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}',
+    connect_args={"sslmode": "require"}
+)
 
 def load_to_db(df, table_name):
     df.to_sql(table_name, engine, if_exists='replace', index=False)
