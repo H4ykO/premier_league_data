f68366866e0b64df3074477e9f01aab4fb209a5c
Joao Lucas Melo <h4yko@Macbook.local>
2026-05-05 20:42:46 -0300

    fix actions

--- model/predict.py
+++ model/predict.py
@@ -17,12 +17,10 @@ DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 
-engine = create_engine(
-    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
-)
-
-
 def load_data():
+    engine = create_engine(
+        f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
+    )
     matches = pd.read_sql('SELECT * FROM matches ORDER BY date ASC', engine)
     matches['date'] = pd.to_datetime(matches['date'])
     return matches
